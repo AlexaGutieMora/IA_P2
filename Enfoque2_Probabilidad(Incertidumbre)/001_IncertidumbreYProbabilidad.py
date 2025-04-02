c8d74f51431934fd42110c42430724efa545c3c9
AlexaGutieMora <143145315+AlexaGutieMora@users.noreply.github.com>
2025-04-01 22:34:04 -0600

    Add files via upload

---
+++ Enfoque2_Probabilidad(Incertidumbre)/001_IncertidumbreYProbabilidad.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  1 22:32:06 2025
+
+@author: k
+"""
+
+##########INCERTIDUMBRE Y PROBABILIDAD
---
+++ Enfoque3_Logica/001_LogicaProposicional.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  1 22:33:03 2025
+
+@author: k
+"""
+
+############## LÓGICA PROPOSICIONAL 
