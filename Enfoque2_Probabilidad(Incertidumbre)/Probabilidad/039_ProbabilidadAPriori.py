d228546a6ccbcb79b7555eb6dbc2826d5ec9cac6
AlexaGutieMora <143145315+AlexaGutieMora@users.noreply.github.com>
2025-05-03 17:45:02 -0600

    Add files via upload

---
+++ Enfoque2_Probabilidad(Incertidumbre)/Probabilidad/039_ProbabilidadAPriori.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  3 16:32:37 2025
+
+@author: k
+"""
+############## 39.PROBABILIDAD A PRIORI
+############ TEMA: PROBABILIDAD
+############ ENFOQUE: PROBABILIDAD (INCERTIDUMBRE)
+
+# Probabilidad a priori: antes de observar datos
+# Supongamos que hay una bolsa con 3 bolas rojas y 2 bolas azules
+
+total_bolas = 5
+prob_roja = 3 / total_bolas
+prob_azul = 2 / total_bolas
+
+print(f"Probabilidad a priori de sacar una bola roja: {prob_roja}")
+print(f"Probabilidad a priori de sacar una bola azul: {prob_azul}")
